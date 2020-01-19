9d43d937013fae38e02ca3ad61eac3395d48ea70
prakash <prakashash18@gmail.com>
2020-01-19 17:59:09 +0800

    Job 8  Errors 1 to 7

---
+++ job8_error6/job8_error6.ino
@@ -0,0 +1,13 @@
+double divideTwoNumbers(double num1, double num2){
+
+     return num1 / num2
+}
+
+void setup() {
+    double result = divideTwoNumbers(7,2)
+    Serial.print(result);
+}
+
+void loop() {
+
+}
